8d007d32a6c4e8414e96f441c49d229f8e7b2996
Anadon <joshua.r.marshall.1991@gmail.com>
2025-05-03 20:59:28 -0400

    Django 5 update issue identified and further fixes.

--- fm_eventmanager/urls.py
+++ fm_eventmanager/urls.py
@@ -1,6 +1,7 @@
 from django.conf import settings
-from django.conf.urls import include, re_path
+from django.conf.urls import include
 from django.contrib import admin
+from django.urls import re_path
 from django.views.generic import RedirectView
 from django.http import HttpResponse
 
