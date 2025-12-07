2fbb765b493ab0d46f536aa89cfb86651921eca7
Chris Burdick <christopher.m.burdick@gmail.com>
2025-12-06 19:02:58 -0500

    #24: Change use of re_path in root urls.py to path

--- fm_eventmanager/urls.py
+++ fm_eventmanager/urls.py
@@ -1,26 +1,26 @@
 from django.conf import settings
 from django.conf.urls import include
 from django.contrib import admin
-from django.urls import re_path
+from django.urls import path
 from django.views.generic import RedirectView
 from django.http import HttpResponse
 
 admin.autodiscover()
 
 urlpatterns = [
-    re_path(
-        r"robots.txt",
+    path(
+        "robots.txt",
         lambda x: HttpResponse("User-Agent: *\n\nDisallow: /", content_type="text/plain"),
         name="robots_file"
     ),
-    re_path(r"^registration/", include("registration.urls", namespace="registration")),
-    re_path(r"^admin/", admin.site.urls),
-    re_path(r"^$", RedirectView.as_view(url="registration"), name="root"),
+    path("registration/", include("registration.urls", namespace="registration")),
+    path("admin/", admin.site.urls),
+    path("", RedirectView.as_view(url="registration"), name="root"),
 ]
 
 if settings.DEBUG:
     import debug_toolbar
 
     urlpatterns = [
-        re_path(r"^__debug__/", include(debug_toolbar.urls)),
+        path("__debug__/", include(debug_toolbar.urls)),
     ] + urlpatterns
