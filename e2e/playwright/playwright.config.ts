64ded4eba668de375886f36f0d07bce8637408a9
Anadon <2878251-anadon@users.noreply.gitlab.com>
2026-04-13 00:23:25 -0400

    Reducing the number of retries Playwright performs.

--- e2e/playwright/playwright.config.ts
+++ e2e/playwright/playwright.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   expect: { timeout: 10_000 },
   fullyParallel: false,
   workers: 1,
-  retries: process.env.CI ? 2 : 0,
+  retries: 0,
   reporter: [
     ["list"],
     ["html", { outputFolder: "playwright-report", open: "never" }],
